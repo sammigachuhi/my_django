b8505b26d18d76014a608e474b1a7b37f3f9f5df
sammigachuhi <sammigachuhi@gmail.com>
2025-05-22 22:22:46 +0300

    postgresql failed

--- sanitation/sanitation/settings.py
+++ sanitation/sanitation/settings.py
@@ -83,6 +83,15 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'postgres',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'postgres',
+    #     'HOST': 'localhost',
+    #     'PORT': '',
+    # }
 }
 
 
